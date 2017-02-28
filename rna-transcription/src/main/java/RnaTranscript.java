0e8724167a44d57f64dd5ab0be52d7af4729b5e9
David Evander <davidevander@gmail.com>
2017-02-28 11:09:26 -0500

    LET'S TRY IT AGAIN

---
+++ rna-transcription/src/main/java/RnaTranscript.java
@@ -0,0 +1,9 @@
+public class RnaTranscription{
+	public static String ofDna(String dna){
+		for (int i=0; i<dna.length; i++){
+			if (dna.charAt == "" || dna == null)
+				return "";
+		}
+	}
+}	
+	
