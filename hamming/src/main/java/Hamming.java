fa48e74d18892bb1c7922e5a200c7f09012426cc
David Evander <davidevander@gmail.com>
2017-02-28 22:14:30 -0500

    passed tests

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,2 +1,14 @@
-public class Hamming {
+public class Hamming{
+	public static int compute(String x, String y) throws IllegalArgumentException{
+		if (x.length()!=y.length()){
+			throw new IllegalArgumentException();
+		}
+		if (x.compareTo(y)==0)
+			return 0;
+		int counter=0;
+		for (int i=0;i<x.length();i++)
+			if (x.charAt(i) != y.charAt(i))
+				counter++;
+		return counter;
+	}
 }
--- hamming/src/test/java/HammingTest.java
+++ hamming/src/test/java/HammingTest.java
@@ -17,51 +17,51 @@ public class HammingTest {
         assertThat(Hamming.compute("A", "A"), is(0));
     }
 
-    @Ignore
+
     @Test
     public void testHammingDistanceForSingleNucleotideStrand() {
         assertThat(Hamming.compute("A", "G"), is(1));
     }
 
-    @Ignore
+
     @Test
     public void testHammingDistanceForSmallStrand() {
         assertThat(Hamming.compute("AG", "CT"), is(2)); 
     }
 
-    @Ignore
+
     @Test
     public void testSmallHammingDistance() {
         assertThat(Hamming.compute("AT", "CT"), is(1));
     }
 
-    @Ignore
+
     @Test
     public void testSmallHammingDistanceInLongerStrand() {
         assertThat(Hamming.compute("GGACG", "GGTCG"), is(1));
     }
 
-    @Ignore
+
     @Test
     public void testValidatesFirstStrandNotLonger() {
         thrown.expect(IllegalArgumentException.class);
         Hamming.compute("AAAG", "AAA");                
     }
 
-    @Ignore
+
     @Test
     public void testValidatesOtherStrandNotLonger() {
         thrown.expect(IllegalArgumentException.class);
         Hamming.compute("AAA", "AAAG");
     }
 
-    @Ignore
+
     @Test
     public void testLargeHammingDistance() {
         assertThat(Hamming.compute("GATACA", "GCATAA"), is(4));
     }
 
-    @Ignore
+
     @Test
     public void testHammingDistanceInVeryLongStrand() {
         assertThat(Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"), is(9));
